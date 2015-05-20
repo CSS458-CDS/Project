6c2972876d63631587fdc0a52c3058ec12c0c661
zsqiii <zhangspp199267@gmail.com>
2015-05-19 17:00:03 -0700

    Update global variables

--- globals.py
+++ globals.py
@@ -5,6 +5,8 @@ COURSE_FILE = "list_of_classes.xlsx"
 COURSE_FILE_FIRST = 9
 #Last Line of relevant information in course file
 COURSE_FILE_LAST = 100
+# of all the graduated student, the percentage of students becoming master student
+MASTER_STUDENT_PERCENT = 0.1
 
 #Course catalog. List of all the courses offered in Autum.
 AUTCAT = []
@@ -14,6 +16,7 @@ WINCAT = []
 SPRCAT = []
 #Course catalog. List of all the courses offered in Summer.
 SUMCAT = []
-
+#elective Course catalog. List of all the elective courses offered in a year, courses are represented as int
+ELCCAT = []
 def initialize_globals():
     courseDriver.buildClasses(COURSE_FILE)
