327970dfc80e5ca16bc2bfbf822164ba7ec08d20
Dan Heizer <danheizer@live.com>
2015-05-20 18:10:06 -0700

    Better IO

--- courseDriver.py
+++ courseDriver.py
@@ -21,6 +21,10 @@ File format:
         K for winter
         P for Spring
 
+    Every course has a 3 number identify
+    Sometimes there's a section, sometimes there's a prefix
+
+
 """
 def buildClasses(fileName=g.COURSE_FILE):
     wb = load_workbook(fileName)
@@ -28,42 +32,113 @@ def buildClasses(fileName=g.COURSE_FILE):
     #Build all summer courses (starting in column A)
     for row in range(g.COURSE_FILE_FIRST,g.COURSE_FILE_LAST):
         if ws["A"+str(row)] is not None:
-            num = ws["A"+str(row)].value
-            time = ws["B"+str(row)].value
-            day = ws["C"+str(row)].value
-            cap = ws["D"+str(row)].value
-            result = c.Course(0, num,time,day,cap)
-            if result.num != -1:
-                g.SUMCAT.append(result)
+            result = make_one_course(ws, 'A', row)
+            if result is not None:
+                if result.num != -1:
+                    result.getTitle()
+                    g.SUMCAT.append(result)
 
     #Build all Autumn courses (starting in column F)
     for row in range(g.COURSE_FILE_FIRST,g.COURSE_FILE_LAST):
-        if ws["F"+str(row)] is not None:
-            num = ws["F"+str(row)].value
-            time = ws["G"+str(row)].value
-            day = ws["H"+str(row)].value
-            cap = ws["I"+str(row)].value
-            result = c.Course(0, num,time,day,cap)
-            if result.num != -1:
-                g.AUTCAT.append(result)
+            if ws["F"+str(row)] is not None:
+                result = make_one_course(ws, 'F', row)
+            if result is not None:
+                if result.num != -1:
+                    result.getTitle()
+                    g.AUTCAT.append(result)
     #Build all Winter courses (starting in column K)
     for row in range(g.COURSE_FILE_FIRST,g.COURSE_FILE_LAST):
         if ws["K"+str(row)] is not None:
-            num = ws["K"+str(row)].value
-            time = ws["L"+str(row)].value
-            day = ws["M"+str(row)].value
-            cap = ws["N"+str(row)].value
-            result = c.Course(0, num,time,day,cap)
-            if result.num != -1:
-                g.WINCAT.append(result)
+            result = make_one_course(ws, "K", row)
+            if result is not None:
+                if result.num != -1:
+                    result.getTitle()
+                    g.WINCAT.append(result)
     #Build all Spring courses (starting in column P)
     for row in range(g.COURSE_FILE_FIRST,g.COURSE_FILE_LAST):
         if ws["P"+str(row)] is not None:
-            num = ws["P"+str(row)].value
-            time = ws["Q"+str(row)].value
-            day = ws["R"+str(row)].value
-            cap = ws["S"+str(row)].value
-            result = c.Course(0, num,time,day,cap)
-            if result.num != -1:
-                g.SPRCAT.append(result)
+            result = make_one_course(ws, 'P', row)
+            if result is not None:
+                if result.num != -1:
+                    result.getTitle()
+                    g.SPRCAT.append(result)
 
+
+def make_one_course(ws, startColumn, startRow):
+    if ws[startColumn+str(startRow)] is None:
+        return None
+
+    row = startRow
+    numCol = startColumn
+    timeCol = chr(ord(startColumn)+1)
+    dayCol = chr(ord(startColumn)+2)
+    capCol = chr(ord(startColumn)+3)
+    num = ws[numCol+str(row)].value
+    time = ws[timeCol+str(row)].value
+    day = ws[dayCol+str(row)].value
+    cap = ws[capCol+str(row)].value
+    result = c.Course(0, num,time,day,cap)
+    #Nicely behaving course number
+    if type(result.num) == int:
+        result.dept = "CSS"
+        return result
+    #Course number with anomalies (csskl, bcusp and the like)
+    else:
+        #Has parens for some reason
+        if ")" in result.num:
+            noparens = result.num[0:result.num.index("(")]
+            result.num = noparens.strip()
+        #Special case: Ends with a letter (section)
+        if 65 <= ord(result.num[len(result.num) - 1]) <= 90:
+            #Take the last char as the section
+            result.section = result.num[len(result.num) - 1]
+            #Remove the char from the number string
+            result.num = result.num[0:(len(result.num)-1)]
+
+
+        #Result just had a section, not a department
+        if len(result.num) == 3:
+            result.num = int(result.num)
+            result.dept = "CSS"
+            return result
+        deptstr = result.num[0:3]
+        if deptstr == "SKL":
+            result.dept = "CSSSKL"
+            result.num = int(result.num[3:len(result.num)])
+            return result
+        deptstr = result.num[0:5]
+        if deptstr == "BCUSP":
+            result.dept = "BCUSP"
+            result.num = int(result.num[5:len(result.num)])
+            return result
+#       If it isnt skl or bcsup, it's css
+        result.dept = "CSS"
+        stripped = result.num.strip()
+        if len(stripped) == 3:
+            result.num = int(stripped)
+            return result
+        #Case: Lab
+        if "Lab" in result.num:
+            result.num = result.num[0:3]
+            result.lab = True
+            return result
+        #Case: Women in stem is offered as 2 different courses, but is one course
+        if result.num == "205/405":
+            result.num = 405
+            return result
+        print("Error reading course: ", end="", sep="")
+        result.display()
+
+
+
+buildClasses()
+courseNums = []
+for i in range(len(g.AUTCAT)):
+    courseNums.append(g.AUTCAT[i].num)
+for i in range(len(g.WINCAT)):
+    courseNums.append(g.WINCAT[i].num)
+for i in range(len(g.SPRCAT)):
+    courseNums.append(g.SPRCAT[i].num)
+for i in range(len(g.SUMCAT)):
+    courseNums.append(g.SUMCAT[i].num)
+courseNums = set(courseNums)
