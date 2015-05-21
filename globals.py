c80a66c08fa55df24a55bc75db754db04e63c793
Dan Heizer <danheizer@live.com>
2015-05-20 20:16:27 -0700

    Update globals.py

--- globals.py
+++ globals.py
@@ -8,7 +8,7 @@ COURSE_FILE_LAST = 100
 # of all the graduated student, the percentage of students becoming master student
 MASTER_STUDENT_PERCENT = 0.1
 
-CORE_CLASSES = {301, 342, 343, 350, 360, 370, 422, 430}
+CORE_CLASSES = {161, 162, 301, 342, 343, 350, 360, 370, 422, 430}
 #Course catalog. List of all the courses offered in Autum.
 AUTCAT = []
 #Course catalog. List of all the courses offered in Winter.
@@ -18,10 +18,25 @@ SPRCAT = []
 #Course catalog. List of all the courses offered in Summer.
 SUMCAT = []
 #elective Course catalog. List of all the elective courses offered in a year, courses are represented as int
-ELCCAT = []
+ALL_ELECTIVES = []
 
 def initialize_globals():
     import courseDriver as cd
     cd.buildClasses()
-    aut_electives = cd.get_electives_from(AUTCAT)
-    cd.print_catalog(aut_electives)
+    get_all_electives()
+
+def get_all_electives():
+    for i in range(len(AUTCAT)):
+        if AUTCAT[i].num not in ALL_ELECTIVES:
+            ALL_ELECTIVES.append(AUTCAT[i].num)
+    for i in range(len(WINCAT)):
+        if WINCAT[i].num not in ALL_ELECTIVES:
+            ALL_ELECTIVES.append(WINCAT[i].num)
+    for i in range(len(SPRCAT)):
+        if SPRCAT[i].num not in ALL_ELECTIVES:
+            ALL_ELECTIVES.append(SPRCAT[i].num)
+    for i in range(len(SUMCAT)):
+        if SUMCAT[i].num not in ALL_ELECTIVES:
+            ALL_ELECTIVES.append(SUMCAT[i].num)
+
+
