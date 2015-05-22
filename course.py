323bf72a8e0e4f8ceaa765c0ebbe671a12a4a15a
Dan Heizer <danheizer@live.com>
2015-05-22 14:05:27 -0700

    Added expertises

--- course.py
+++ course.py
@@ -37,7 +37,7 @@ class Course:
         self.taughtBy = None
         self.field = "Default Field"
         self.section = -1
-        self.dept = "None"
+        self.dept = []
         self.title = "Default Title"
         self.difficulty = -1
         self.quarter = quarter
@@ -92,80 +92,120 @@ class Course:
         if self.dept == "CSS":
             if self.num == 107:
                 self.title = "Introduction to Programming through Animated Storytelling"
+                self.set_field(0)
             elif self.num == 161:
                 self.title = "Fundamentals Of Computing"
+                self.set_field(0)
+
             elif self.num == 162:
                 self.title = "Programming Methodology"
+                self.set_field(0)
             elif self.num == 211:
                 self.title = "Computers and Society"
+                self.set_field(12)
             elif self.num == 225:
                 self.title = "Physics and Chemistry of Computer Components and Their Manufacture"
+                self.set_field(10)
             elif self.num == 290:
                 self.title = "Topics in Computing"
+                self.set_field(12)
             elif self.num == 295:
                 self.title = "K-12 Computing Education"
+                self.set_field(9)
             elif self.num == 301:
                 self.title = "Technical Writing"
+                self.set_field(6)
             elif self.num == 310:
                 self.title = "Information Assurance and Cyber Security"
+                self.set_field(5)
             elif self.num == 332:
                 self.title = "Programming Issues with Object-Oriented Languages"
+                self.set_field(0)
             elif self.num == 337:
                 self.title = "Secure Systems"
+                self.set_field(5)
             elif self.num == 341:
                 self.title = "Fundamentals of Programming Theory and Applications"
+                self.set_field(0)
             elif self.num == 342:
                 self.title = "Data Structures, Algorithms, and Discrete Mathematics I"
+                self.set_field(0)
             elif self.num == 343:
                 self.title = "Data Structures, Algorithms, and Discrete Mathematics II"
+                self.set_field(0)
             elif self.num == 350:
                 self.title = "Management Principles for Computing Professionals"
+                self.set_field(9)
             elif self.num == 360:
                 self.title = "Software Engineering"
+                self.set_field(2)
             elif self.num == 370:
                 self.title = "Analysis and Design"
+                self.set_field(2)
             elif self.num == 371:
                 self.title = "The Business of Technology"
+                self.set_field(12)
             elif self.num == 383:
                 self.title = "Bioinformatics"
+                self.set_field(1)
             elif self.num == 385:
                 self.title = "Introduction to Game Development"
+                self.set_field(11)
             elif self.num == 405:
                 self.title = "Women in STEM Seminar: Career/Professional Life"
+                self.set_field(12)
             elif self.num == 411:
                 self.title = "Computing Technology and Public Policy"
+                self.set_field(12)
             elif self.num == 415:
                 self.title = "Emerging Topics in Information Assurance and Cybersecurity"
+                self.set_field(5)
+            #TODO
             elif self.num == 421:
                 self.title = "Introduction to Hardware and Operating Systems"
+                self.set_field(4)
+                self.set_field(3)
             elif self.num == 422:
                 self.title = "Hardware and Computer Organization"
+                self.set_field(4)
             elif self.num == 427:
                 self.title = "Introduction to Embedded Systems"
+                self.set_field(4)
             elif self.num == 428:
                 self.title = "Advanced Embedded Systems"
+                self.set_field(4)
             elif self.num == 430:
                 self.title = "Operating Systems"
+                self.set_field(3)
             elif self.num == 432:
                 self.title = "Network Design"
+                self.set_field(8)
             elif self.num == 434:
                 self.title = "Parallel and Distributed Computing"
+                self.set_field(8)
             elif self.num == 448:
                 self.title = "Introduction to Compilers"
+                self.set_field(0)
             elif self.num == 451:
                 self.title = "3-D Computer Graphics"
+                self.set_field(11)
             elif self.num == 455:
                 self.title = "Introduction to Computational Science and Scientific Programming"
+                self.set_field(1)
+            #TODO
             elif self.num == 457:
                 self.title = "Multimedia and Signal Computing"
+                self.set_field(4)
             elif self.num == 458:
                 self.title = "Fundamentals of Computer Simulation Theory and Application"
+                self.set_field(1)
             elif self.num == 473:
                 self.title = "Entrepreneurship Seminar"
             elif self.num == 474:
                 self.title = "Product Development Lab"
             elif self.num == 475:
                 self.title = "Database Systems"
+                self.set_field(7)
             elif self.num == 480:
                 self.title = "480 Principles of Human-Computer Interaction"
             elif self.num == 490:
@@ -182,48 +222,74 @@ class Course:
                 self.title = "Undergraduate Research"
             elif self.num == 501:
                 self.title = "Data Structures and Object-Oriented Programming I"
+                self.set_field(0)
             elif self.num == 502:
                 self.title = "Data Structures and Object-Oriented Programming II"
+                self.set_field(0)
             elif self.num == 503:
                 self.title = "Systems Programming"
+                self.set_field(0)
             elif self.num == 506:
                 self.title = "Software Development Processes"
+                self.set_field(2)
             elif self.num == 507:
                 self.title = "Software Modeling Techniques"
+                self.set_field(2)
             elif self.num == 508:
                 self.title = "Software Testing and Quality"
+                self.set_field(0)
             elif self.num == 514:
                 self.title = "Security, Policy, Ethics, and the Legal Environment"
+                self.set_field(5)
             elif self.num == 517:
                 self.title = "Information Assurance and the Secure Development Lifecycle"
+                self.set_field(2)
+                self.set_field(5)
             elif self.num == 519:
                 self.title = "Incident Response and Recovery"
+                self.set_field(5)
             elif self.num == 527:
                 self.title = "Cryptography and Data Assurance"
+                self.set_field(5)
             elif self.num == 533:
                 self.title = "Distributed Computing"
+                self.set_field(8)
             elif self.num == 537:
                 self.title = "Network and Internet Security"
+                self.set_field(8)
+                self.set_field(5)
             elif self.num == 538:
                 self.title = "Security in Emerging Wireless and Mobile Networks"
+                self.set_field(8)
+                self.set_field(5)
             elif self.num == 552:
                 self.title = "Topics in Rendering"
+                self.set_field(11)
             elif self.num == 553:
                 self.title = "Software Architecture"
+                self.set_field(2)
             elif self.num == 555:
                 self.title = "Evaluating Software Design"
+                self.set_field(2)
             elif self.num == 565:
                 self.title = "Research Methods in Software Development"
+                self.set_field(2)
             elif self.num == 566:
                 self.title = "Software Management"
+                self.set_field(2)
             elif self.num == 572:
                 self.title = "Evidence-Based Design"
+                self.set_field(2)
             elif self.num == 577:
                 self.title = "Secure Software Development"
+                self.set_field(2)
+                self.set_field(5)
             elif self.num == 578:
                 self.title = "Vulnerability Analysis and Detection"
+                self.set_field(5)
             elif self.num == 579:
                 self.title = "Malware and Attack Reverse Engineering"
+                self.set_field(5)
             elif self.num == 590:
                 self.title = "Special Topics in Computing"
             elif self.num != 515 and self.num != 539:
@@ -232,16 +298,28 @@ class Course:
         elif self.dept == "CSSSKL":
             if self.num == 161:
                 self.title = "Fundamental Programming Skills"
+                self.set_field(0)
             elif self.num == 162:
                 self.title = "Programming Methodology Skills"
+                self.set_field(0)
         elif self.dept == "BCUSP":
             if self.num == 161:
                 self.title = "Fundamentals of Computing"
+                self.set_field(0)
             elif self.num == 162:
                 self.title = "Programming Methodology"
+                self.set_field(0)
 
         else:
+            print("No title could be found for:", self.num, end="\t")
             self.display()
+
+#    Sets the field for desired expertise
+#   0:programming
+
+    def set_field(self, num):
+        self.field.append( g.FIELDS[num])
+
 
 """
 Faculty expertise:
