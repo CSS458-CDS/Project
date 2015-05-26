cc7041de70d35ab8b2d1340e9c84e3375cdaaf83
Caleb Hawkins <hawkins.caleb93@gmail.com>
2015-05-26 13:09:49 -0700

    Create facultyCourseScheduler.py
    Work in progress

---
+++ facultyCourseScheduler.py
@@ -0,0 +1,144 @@
+from faculty import faculty
+
+expertList = ["Programming", "Cybersecurity", "Software Engineering"]
+
+newFac = faculty("Hawkins", "Y", 12, 5, expertList)
+
+def facultyCourseScheduler(fac):
+    highPrio = [] # Designates classes that the faculty member is exceptional in
+    medPrio = [] # Designates classes that the faculty member can teach
+    lowPrio = [] # Designates classes that the faculty member should not teach or
+                 # are outside of their range of expertise
+    
+    populateLowPrio(lowPrio)
+    
+    if "Programming" in fac.getExpertise():
+        classList = [107,161,162,332,341,342,343,360,385,421,422,427,428,430,432,\
+                    448,455,458,474,475,501,502,503,506,507,508,533,553,555,565,\
+                    566,572,577]
+                    
+        for i in range(len(classList)):
+            if classList[i] not in medPrio:
+                medPrio.append(classList[i])
+            
+            if classList[i] in lowPrio:
+                lowPrio.remove(classList[i])
+                
+    if "Scientific Computing" in fac.getExpertise():
+        classList = [107,161,162,211,225,290,332,337,341,342,343,350,360,370,371,\
+                    385,411,421,422,427,428,430,432,434,455,458,475,480,490,495,\
+                    496,497,498,499,501,502,503,506,507,508,517,533,537,552,553,\
+                    555,565,566,577,590]
+                    
+        for i in range(len(classList)):
+            if classList[i] not in medPrio:
+                medPrio.append(classList[i])
+            
+            if classList[i] in lowPrio:
+                lowPrio.remove(classList[i])
+                
+    if "Scientific Engineering" in fac.getExpertise():
+        classList = [107,161,162,211,225,290,332,337,341,342,343,350,360,370,371,\
+                    385,411,421,422,427,428,430,432,434,455,458,475,480,490,495,\
+                    496,497,498,499,501,502,503,506,507,508,517,533,537,552,553,\
+                    555,565,566,577,590]
+                    
+        for i in range(len(classList)):
+            if classList[i] not in medPrio:
+                medPrio.append(classList[i])
+            
+            if classList[i] in lowPrio:
+                lowPrio.remove(classList[i])
+                
+    if "Cybersecurity" in fac.getExpertise():
+        pass
+        
+    if "Writing" in fac.getExpertise():
+        pass
+    
+    if "Databases" in fac.getExpertise():
+        pass
+        
+    if "Hardware" in fac.getExpertise():
+        pass
+        
+    if "Networking" in fac.getExpertise():
+        pass
+        
+    if "Operating Systems" in fac.getExpertise():
+        pass
+        
+    if "Teaching" in fac.getExpertise():
+        pass
+    
+def populateLowPrio(lowPrio):
+    lowPrio.append(107) # Introduction to Programming through Animated Storytelling
+    lowPrio.append(161) # Fundamentals Of Computing
+    lowPrio.append(162) # Programming Methodology
+    lowPrio.append(211) # Computers and Society
+    lowPrio.append(225) # Physics and Chemistry of Computer Components and Their Manufacture
+    lowPrio.append(290) # Topics in Computing
+    lowPrio.append(295) # K-12 Computing Education
+    lowPrio.append(301) # Technical Writing
+    lowPrio.append(310) # Information Assurance and Cyber Security
+    lowPrio.append(332) # Programming Issues with Object-Oriented Languages
+    lowPrio.append(337) # Secure Systems
+    lowPrio.append(341) # Fundamentals of Programming Theory and Applications
+    lowPrio.append(342) # Data Structures, Algorithms, and Discrete Mathematics I
+    lowPrio.append(343) # Data Structures, Algorithms, and Discrete Mathematics II
+    lowPrio.append(350) # Management Principles for Computing Professionals
+    lowPrio.append(360) # Software Engineering
+    lowPrio.append(370) # Analysis and Design
+    lowPrio.append(371) # The Business of Technology
+    lowPrio.append(383) # Bioinformatics
+    lowPrio.append(385) # Introduction to Game Development
+    lowPrio.append(405) # Women in STEM Seminar: Career/Professional Life
+    lowPrio.append(411) # Computing Technology and Public Policy
+    lowPrio.append(415) # Emerging Topics in Information Assurance and Cybersecurity
+    lowPrio.append(421) # Introduction to Hardware and Operating Systems
+    lowPrio.append(422) # Hardware and Computer Organization
+    lowPrio.append(427) # Introduction to Embedded Systems
+    lowPrio.append(428) # Advanced Embedded Systems
+    lowPrio.append(430) # Operating Systems
+    lowPrio.append(432) # Network Design
+    lowPrio.append(434) # Parallel and Distributed Computing
+    lowPrio.append(448) # Introduction to Compilers
+    lowPrio.append(451) # 3-D Computer Graphics
+    lowPrio.append(455) # Introduction to Computational Science and Scientific Programming
+    lowPrio.append(457) # Multimedia and Signal Computing
+    lowPrio.append(458) # Fundamentals of Computer Simulation Theory and Application
+    lowPrio.append(473) # Entrepreneurship Seminar
+    lowPrio.append(474) # Product Development Lab
+    lowPrio.append(475) # Database Systems
+    lowPrio.append(480) # 480 Principles of Human-Computer Interaction
+    lowPrio.append(490) # Special Topics in Computing and Software Systems
+    lowPrio.append(495) # Applied Computing Internship
+    lowPrio.append(496) # Applied Computing Capstone
+    lowPrio.append(497) # Computer Science and Software Engineering Capstone
+    lowPrio.append(498) # Independent Study
+    lowPrio.append(499) # Undergraduate Research
+    lowPrio.append(501) # Data Structures and Object-Oriented Programming I
+    lowPrio.append(502) # Data Structures and Object-Oriented Programming II
+    lowPrio.append(503) # Systems Programming
+    lowPrio.append(506) # Software Development Processes
+    lowPrio.append(507) # Software Modeling Techniques
+    lowPrio.append(508) # Software Testing and Quality
+    lowPrio.append(514) # Security, Policy, Ethics, and the Legal Environment
+    lowPrio.append(517) # Information Assurance and the Secure Development Lifecycle
+    lowPrio.append(519) # Incident Response and Recovery
+    lowPrio.append(527) # Cryptography and Data Assurance
+    lowPrio.append(533) # Distributed Computing
+    lowPrio.append(537) # Network and Internet Security
+    lowPrio.append(538) # Security in Emerging Wireless and Mobile Networks
+    lowPrio.append(552) # Topics in Rendering
+    lowPrio.append(553) # Software Architecture
+    lowPrio.append(555) # Evaluating Software Design
+    lowPrio.append(565) # Research Methods in Software Development
+    lowPrio.append(566) # Software Management
+    lowPrio.append(572) # Evidence-Based Design
+    lowPrio.append(577) # Secure Software Development
+    lowPrio.append(578) # Vulnerability Analysis and Detection
+    lowPrio.append(579) # Malware and Attack Reverse Engineering
+    lowPrio.append(590) # Special Topics in Computing
+    
+facultyCourseScheduler(newFac)
