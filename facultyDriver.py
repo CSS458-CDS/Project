928a28704d95782fbad21bb8b9d5ffda9f23ee3b
Caleb Hawkins <hawkins.caleb93@gmail.com>
2015-05-31 14:46:53 -0700

    Update facultyDriver.py

--- facultyDriver.py
+++ facultyDriver.py
@@ -27,32 +27,33 @@ def buildFaculty(fileName=g.FACULTY_FILE):
     ws = wb.active
     
     # Build faculty from first readable row to last
-    row = 5
-    while ws.cell(row, 1).value is not None:  # Rows >= 5 in the file have relevant data
-        lastName = ws.cell(row, 1).value
+    rowNum = 5
+    while ws.cell(row = rowNum, column = 1).value is not None:  # Rows >= 5 in the file have relevant data
+        lastName = ws.cell(row = rowNum, column = 1).value
         
-        if ws.cell(row, 2).value == 'Y':
+        if ws.cell(row = rowNum, column = 2).value == 'Y':
             fullTime = 1
         else:
             fullTime = 0
             
-        numClasses = ws.cell(row, 3).value + 0.
-        studentsAdvised = ws.cell(row, 4).value
+        numClasses = ws.cell(row = rowNum, column = 3).value + 0.
+        studentsAdvised = ws.cell(row = rowNum, column = 4).value
         
-        expertise = [ws.cell(row, 5).value]
+        expertise = [ws.cell(row = rowNum, column = 5).value]
 
-        if ws.cell(row, 6).value != None:
-            expertise.append(ws.cell(row, 6).value)
-        if ws.cell(row, 7).value != None:
-            expertise.append(ws.cell(row, 7).value)
-        if ws.cell(row, 6).value != None:
-            expertise.append(ws.cell(row, 6).value)
-        if ws.cell(row, 7).value != None:
-            expertise.append(ws.cell(row, 7).value)
+        if ws.cell(row = rowNum, column = 6).value != None:
+            expertise.append(ws.cell(row = rowNum, column = 6).value)
+        if ws.cell(row = rowNum, column = 7).value != None:
+            expertise.append(ws.cell(row = rowNum, column = 7).value)
+        if ws.cell(row = rowNum, column = 6).value != None:
+            expertise.append(ws.cell(row = rowNum, column = 6).value)
+        if ws.cell(row = rowNum, column = 7).value != None:
+            expertise.append(ws.cell(row = rowNum, column = 7).value)
             
         g.allFaculty.append(faculty(lastName, fullTime, numClasses, studentsAdvised, expertise))
         
-        row += 1
+        rowNum += 1
+        
     g.ADVISED_STUDENTS_PER_QUARTER = 0
     for i in g.allFaculty:
         g.ADVISED_STUDENTS_PER_QUARTER += i.studentsAdvised
